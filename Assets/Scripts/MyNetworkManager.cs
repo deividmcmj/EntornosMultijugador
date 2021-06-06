e66069bde59722adccaf8e43195eb0151ca3903f
deividmcmj <48552318+deividmcmj@users.noreply.github.com>
2021-06-06 14:21:54 +0200

    Cambios NetworkManager

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -26,6 +26,8 @@ public class MyNetworkManager : NetworkManager
 
     public override void OnClientDisconnect(NetworkConnection conn)
     {
+        _polePositionManager._uiManager.AbandonMenu();
+        _polePositionManager._setupPlayer.StopAllCars();
         if (conn.identity != null)
         {
             _polePositionManager.RemovePlayer(conn.identity.GetComponent<PlayerInfo>());
